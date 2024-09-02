2e53f288eadf2301750efcea66970e12f773bf7e
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-02 13:59:49 +0300

    Create theater app

--- theater_service/wsgi.py
+++ theater_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django-theater-api-service project.
+WSGI config for django-theater-theater-service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-theater-api-service.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django-theater-theater-service.settings')
 
 application = get_wsgi_application()
