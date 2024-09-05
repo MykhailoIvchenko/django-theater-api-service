fa9e78b8228db852fd23d1fedd4fe3aa88f0a15b
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-05 11:50:18 +0300

    Add genre model

--- theater/models.py
+++ theater/models.py
@@ -1,2 +1,14 @@
-from django.db import models
+import os
+import uuid
 
+from django.core.exceptions import ValidationError
+from django.db import models
+from django.conf import settings
+from django.utils.text import slugify
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
