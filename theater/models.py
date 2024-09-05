421c54ccecc5aebcc412cf2832a0df776b383503
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-05 11:59:55 +0300

    Add a reservation model

--- theater/models.py
+++ theater/models.py
@@ -62,3 +62,17 @@ class Performance(models.Model):
 
     def __str__(self):
         return self.play.title + " " + str(self.show_time)
+
+
+class Reservation(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE
+    )
+
+    def __str__(self):
+        return str(self.created_at)
+
+    class Meta:
+        ordering = ["-created_at"]
+
