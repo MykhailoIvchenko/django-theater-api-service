c5ba8d0b593c7b934825e525fee13f4addbd033f
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-05 12:33:26 +0300

    Add view for the genre

--- theater/views.py
+++ theater/views.py
@@ -1,3 +1,37 @@
-from django.shortcuts import render
+from datetime import datetime
 
-# Create your views here.
+from django.db.models import F, Count
+from drf_spectacular.types import OpenApiTypes
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import action
+from rest_framework.pagination import PageNumberPagination
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.response import Response
+from rest_framework.viewsets import GenericViewSet
+
+from theater.models import Genre, Actor, TheaterHall, Play, Performance, Reservation
+from theater.permissions import IsAdminOrIfAuthenticatedReadOnly
+
+from theater.serializers import (
+    GenreSerializer,
+    ActorSerializer,
+    TheaterHallSerializer,
+    PlaySerializer,
+    PerformanceSerializer,
+    PerformanceListSerializer,
+    PlayDetailSerializer,
+    PerformanceDetailSerializer,
+    PlayListSerializer,
+    ReservationSerializer,
+    ReservationListSerializer,
+)
+
+class GenreViewSet(
+    mixins.CreateModelMixin,
+    mixins.ListModelMixin,
+    GenericViewSet,
+):
+    queryset = Genre.objects.all()
+    serializer_class = GenreSerializer
+    permission_classes = (IsAdminOrIfAuthenticatedReadOnly,)
