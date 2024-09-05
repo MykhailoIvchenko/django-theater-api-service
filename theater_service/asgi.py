ef8063b5d3f71eaa6605ee73bc03025b7f649f19
MykhailoIvchenko <ivchenkomykhailo@gmail.com>
2024-09-05 12:07:38 +0300

    Fix the main service name link

--- theater_service/asgi.py
+++ theater_service/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django-theater-theater-service project.
+ASGI config for django-theater-service project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
--- theater_service/wsgi.py
+++ theater_service/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for django-theater-theater-service project.
+WSGI config for django-theater-service project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
